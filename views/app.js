58e23a26c4a1189494ccf10b8dcdf96b2e50bd0f
Kate_Nguyen <Kate_nguyen>
2026-02-10 14:52:56 -0800

    added

--- views/app.js
+++ views/app.js
@@ -6,6 +6,11 @@ const app = express();
 
 // Port
 const PORT = 3005;
+//array of users
+const submit =[];
+//middleware to parse JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from public
 app.use(express.static("public"));
@@ -13,6 +18,11 @@ app.use(express.static("public"));
 // Home route
 app.get("/", (req, res) => {
   res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+app.post('/submit', (req, res) => {
+  submit.push(req.body);
+  res.sendFile(`${import.meta.dirname}/views/submit-form.html`);
 });
 
 // Start server
