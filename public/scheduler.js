5f8d3a603644428e643ad31c7445d45457e216dd
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-02-10 14:51:19 -0800

    js changes

--- public/scheduler.js
+++ public/scheduler.js
@@ -1,18 +1,20 @@
-document.getElementById("contactForm").onsubmit = () => {
+document.getElementById("appointment-form").onsubmit = () => {
 
     clearErrors();
 
     let isValid = true;
-    
+
     let firstName = document.getElementById("fname").value.trim();
     if (firstName === "") {
-        document.getElementById("fnameError").textContent = "First name is required.";
+        document.getElementById("err-fname").textContent = "First name is required.";
         isValid = false;
     }
 
     let lastName = document.getElementById("lname").value.trim();
     if (lastName === "") {
-        document.getElementById("lnameError").textContent = "Last name is required.";
+        document.getElementById("err-lname").textContent = "Last name is required.";
         isValid = false;
     }
-}
+
+    return isValid; 
+};
